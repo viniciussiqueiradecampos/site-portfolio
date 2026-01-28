92c0f186fdb35ebb263be3409444cb71039d78d5
Vinicius Campos <vinisiqueiradecampos@gmail.com>
2026-01-28 22:18:50 +0000

    Identity check: Added version 4.1 log to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+console.log("Portfolio Version: 4.1 - ACTIVE (Force Refresh)");
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
