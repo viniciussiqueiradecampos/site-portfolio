8c1e39bf99b84cc2725190fa7eefcf5a7baee64a
Vinicius Campos <vinisiqueiradecampos@gmail.com>
2026-02-19 17:31:07 +0000

    test: disable CSP to verify media loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-console.log("Portfolio Version: 4.6 - CSP FORCE MODE");
+console.log("Portfolio Version: 4.7 - CSP DISABLED TEST");
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
