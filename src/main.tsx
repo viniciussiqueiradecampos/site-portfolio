c7e82660304f9c59c862010a364f9c86e1211382
Vinicius Campos <vinisiqueiradecampos@gmail.com>
2026-02-19 17:24:38 +0000

    fix: ultra permissive CSP mode for debugging media display

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-console.log("Portfolio Version: 4.1 - ACTIVE (Force Refresh)");
+console.log("Portfolio Version: 4.6 - CSP FORCE MODE");
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
